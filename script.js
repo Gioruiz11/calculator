6f346e246e29705bc36cd66c0541b2e2e0e2cf09
Gioruiz11 <gioruiz11@hotmail.com>
2022-06-10 19:11:50 -0400

    d

--- script.js
+++ script.js
@@ -42,25 +42,37 @@ function showUp(clickedElement){
 function operator(clickedElement){
     const display = document.getElementsByClassName("display")
     values.push(parseInt(display[0].textContent));
+
+    // when this is called for a second time, evaluate
+    // the 3 values, then save the result as the first value
+    // in the next expression
+    if(values.length === 3){
+        let result = operate(values[0], values[1], values[2]);
+        values = [];
+        values.push(result);
+        console.log("hello");
+    }
+
     values.push(clickedElement.textContent);
     display[0].textContent = "";
     console.log(values);
-    // TODO: when this is called for a second time, evaluate
-    // the 3 values, then save the result as the first value
-    // in the next expression
-    if(values.length == 3){
-        operate(values[0], values[1], values[2]);
-    }
 }
+
 // operates on the two numbers being input, saves the result,
 // and shows the result on the screen
 function equals(){
     const display = document.getElementsByClassName("display");
+
     values.push(parseInt(display[0].textContent));
     console.log(operate(values[0], values[1], values[2]));
     let result = operate(values[0], values[1], values[2]);
+
+    // in order to round long decimals to 6 places
+    let roundedResult = Math.round((result + Number.EPSILON) * 1000000) / 1000000;
+
     display[0].textContent = "";
-    display[0].textContent += result;
+    display[0].textContent += roundedResult;
+
 }
 
 // clears the memory and the screen
